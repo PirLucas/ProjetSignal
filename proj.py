5ba1b03bc9315a20e72136144711403cdde87545
Lucas Pironet <l.pironet@students.ephec.be>
2023-11-07 11:13:36 +0100

    Expérimentation avec le coef de correlation

--- proj.py
+++ proj.py
@@ -70,44 +70,91 @@ def get_frame(filename, index):
 
 if __name__ == "__main__":
 
+    #I = plt.imread("projet/r.png");
+    #I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+    #I = np.dot(I[..., :3], [0.2989, 0.5870, 0.1140])
     initFrame = get_frame(VFILE, 1)
+    #initFrame = cv2.cvtColor(initFrame, cv2.COLOR_BGR2GRAY)
+    #initFrame = np.dot(initFrame[..., :3], [0.2989, 0.5870, 0.1140])
+
+    initFrame2 = get_frame(VFILE, 292)
+    initFrame2 = cv2.cvtColor(initFrame2, cv2.COLOR_BGR2GRAY)
+
+    initFrame3 = get_frame(VFILE, 2)
+    initFrame3 = cv2.cvtColor(initFrame3, cv2.COLOR_BGR2GRAY)
     initL1 = []
     initL2 = []
-    for ligne in initFrame[:,400:850]:
-        initL1.append(list(ligne[200]))
-        initL2.append(list(ligne[300]))
+
+    #grayFrame = np.dot(initFrame[..., :3], [0.2989, 0.5870, 0.1140])
+    grayFrame = cv2.cvtColor(initFrame, cv2.COLOR_BGR2GRAY)
+
+    for ligne in grayFrame[:, 400:850]:
+
+        initL1.append(ligne[200])
+        initL2.append(ligne[300])
+
     initL1 = np.array(initL1)
     initL2 = np.array(initL2)
-    #print(np.array(initL1))
+    """
     #print(signal.correlate2d(initL1,initL1))
-    #print(np.corrcoef(initL1, initL1))
+    print(np.corrcoef(grayFrame, grayFrame))
+    #print(initL1)
+    print(np.corrcoef(initL1, initL1))
+    """
+    print(np.corrcoef(grayFrame, grayFrame))
+    print(np.corrcoef(grayFrame, initFrame2))
+    np_subtr = np.subtract(np.corrcoef(grayFrame, grayFrame), np.corrcoef(grayFrame, initFrame3))
+    #np_subtr = np.subtract(initFrame,I)
+    np_mean = np.mean(np_subtr)
 
+    print('numpy mean is:', np_mean)
 
+    #plt.imshow(np.corrcoef(initFrame, I))
+    plt.imshow(np_subtr)
+
+    plt.show()
 
 
     # Loop through each frame in the video using the generator 'get_frames(VFILE)'.
-    for f in get_frames(VFILE):
+    for e,f in enumerate( get_frames(VFILE)):
+        #print(e)
         # If 'f' is None, it means there are no more frames to process, so we break the loop.
         if f is None:
             break
         # Display the current frame using OpenCV's 'imshow' function.
         # The first argument is the window name, which will be displayed at the top of the window.
         # The second argument is the frame to be displayed.
-
+        fO = f
         f = f[:,400:850]
+        f = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+        #f = np.dot(f[..., :3], [0.2989, 0.5870, 0.1140])
+
         f1 = f.copy()
         L1 = []
         L2 = []
+
         for ligne in f1:
-            L1.append(list(ligne[200]))
-            L2.append(list(ligne[300]))
-            ligne[200] = [250,250,250]
+            L1.append(ligne[200])
+            L2.append(ligne[300])
+            ligne[200] = 250
+            ligne[300] = 250
 
         L1 = np.array(L1)
         L2 = np.array(L2)
-        print("L1",np.allclose(L1, initL1,rtol=0, atol=50))
-        print("L2",np.allclose(L2, initL2,rtol=0, atol=50))
-        cv2.imshow('frame', f1)
+        #print("L1",np.allclose(L1, initL1,rtol=0, atol=50))
+        #print("L2",np.allclose(L2, initL2,rtol=0, atol=50))
+        #print(np.corrcoef(L1, initL1))
+        #print(np.corrcoef(initFrame, initFrame))
+
+
+        diff =  np.subtract(np.corrcoef(grayFrame[:,400:850], grayFrame[:,400:850]),np.corrcoef(grayFrame[:,400:850], f))
+        diff2 =  np.corrcoef(grayFrame[:,400:850], f)
+        res = cv2.resize(diff, dsize=(450, 450), interpolation=cv2.INTER_CUBIC)
+        res2 = cv2.resize(diff2, dsize=(450, 450), interpolation=cv2.INTER_CUBIC)
+        two = np.concatenate((diff,diff2), axis=1)
+        cv2.imshow('frame', res)
+        cv2.imshow('frame2', f1)
+         #profile-line?
 
         # Wait for a short time (10 milliseconds) and check for the 'Esc' key press (ASCII code 27).
         # If the 'Esc' key is pressed, we break the loop to stop displaying frames.
